22781ab2a0a9288bebee74284f00571d8be30a7b
KafetzisThomas <tomkafetzis06@gmail.com>
2025-09-22 13:27:43 +0300

    Initial commit

---
+++ src/test/java/com/kafetzisthomas/banking/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafetzisthomas.banking.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
