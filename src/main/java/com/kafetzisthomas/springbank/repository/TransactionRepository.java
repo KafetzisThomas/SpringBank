7a17de61fdb17eb6150b3b5726f47f0d22da265a
KafetzisThomas <tomkafetzis06@gmail.com>
2026-07-14 10:01:22 +0300

    Rename dao to repository

--- src/main/java/com/kafetzisthomas/springbank/repository/TransactionRepository.java
+++ src/main/java/com/kafetzisthomas/springbank/repository/TransactionRepository.java
@@ -1,4 +1,4 @@
-package com.kafetzisthomas.springbank.dao;
+package com.kafetzisthomas.springbank.repository;
 
 import com.kafetzisthomas.springbank.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +9,10 @@ import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    // retrieve a list of transactions associated with the specified owner's email
     List<Transaction> findAllByOwnerEmail(String ownerEmail);
 
-    // retrieve transactions for the given email with timestamp in the inclusive range (start,end)
     List<Transaction> findAllByOwnerEmailAndTimestampBetween(String email, LocalDateTime start, LocalDateTime end);
 
-    // find most recent transaction for the given owner by timestamp for balance calculations
     Optional<Transaction> findTopByOwnerEmailOrderByTimestampDesc(String ownerEmail);
 
 }
